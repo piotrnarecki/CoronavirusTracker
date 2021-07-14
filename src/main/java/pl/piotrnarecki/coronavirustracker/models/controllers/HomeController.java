3f6073b034c0d9e9f48efc6a266fb50ab828609e
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-07-14 10:21:26 +0200

    added displaying data in table

--- src/main/java/pl/piotrnarecki/coronavirustracker/models/controllers/HomeController.java
+++ src/main/java/pl/piotrnarecki/coronavirustracker/models/controllers/HomeController.java
@@ -1,16 +1,26 @@
 package pl.piotrnarecki.coronavirustracker.models.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.piotrnarecki.coronavirustracker.services.CoronaVirusDataService;
 
 // odpowiada za UI
 
 @Controller
 public class HomeController {
 
+@Autowired
+CoronaVirusDataService coronaVirusDataService;
+
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+
+
+        model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
         return "home";
     }
 
--- src/main/java/pl/piotrnarecki/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/pl/piotrnarecki/coronavirustracker/services/CoronaVirusDataService.java
@@ -65,5 +65,7 @@ public class CoronaVirusDataService {
 
     }
 
-
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
 }
